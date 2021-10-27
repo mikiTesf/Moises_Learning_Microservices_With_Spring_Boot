f643f98009a62df09a7e3af126a4d49cc26b2abc
Mikyas Tesfamichael <mickyastesfamichael@gmail.com>
2021-10-27 06:23:12 +0300

    chapter 8: added a Gateway service

--- frontend/src/services/ChallengesApiClient.js
+++ frontend/src/services/ChallengesApiClient.js
@@ -1,6 +1,6 @@
 class ChallengesApiClient {
 
-    static SERVER_URL = 'http://localhost:8080';
+    static SERVER_URL = 'http://localhost:8000';
     static GET_CHALLENGE = '/challenges/random';
     static POST_RESULT = '/attempts';
     static GET_ATTEMPTS_BY_ALIAS = '/attempts?alias=';
